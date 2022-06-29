da761c87f4f67e257ca402fb554fed88493de4d3
Chensi2113 <88916631+Chensi2113@users.noreply.github.com>
2022-06-29 16:13:47 +0800

    初始化

---
+++ src/main/java/com/example/bwmz/BwMzApplication.java
@@ -0,0 +1,13 @@
+package com.example.bwmz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BwMzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BwMzApplication.class, args);
+    }
+
+}
